108dafbb9214dd15e1e6b1350f49245938a97423
nikita-p <nikita.petrov.1997@gmail.com>
2020-03-31 22:46:16 +0700

    Add new tests

--- tests/test_DataPreprocessing.py
+++ tests/test_DataPreprocessing.py
@@ -13,3 +13,21 @@ def test_axis_size():
     dp = DataPreprocessing()
     edges = dp.get_axis_size(1, 3, 2, 3, 0.1, 10)
     assert len(edges) == 30
+
+
+def test_parsing():
+    dp = DataPreprocessing()
+    dp.parse_pickle("tests/DataPreprocessing_example_data.pkl")
+    assert len(dp.X) == 10
+    assert len(dp.y) == 10
+
+
+def test_board_generation():
+    dp = DataPreprocessing()
+    dp.parse_pickle("tests/DataPreprocessing_example_data.pkl")
+    imgs, boxes, masks = dp.generate_boards(100, 1, 10)
+    assert len(imgs) == len(masks)
+    assert len(boxes) == len(imgs)
+    assert len(boxes) == 10
+    assert imgs[0].shape == (100, 100)
+    assert len(boxes[0]) == 1
