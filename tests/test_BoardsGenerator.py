7a7a6a61fbfa97bff1f693fd481d4155aaa6b2c3
nikita-p <nikita.petrov.1997@gmail.com>
2020-10-15 19:26:22 +0700

    Add test tree and fix test

--- tests/test_BoardsGenerator.py
+++ tests/test_BoardsGenerator.py
@@ -4,17 +4,13 @@ from farichlib import BoardsGenerator
 
 @pytest.mark.parametrize(
     "n_boards, sizeX, sizeY",
-    [(0, 100, 432), (10, 14, 12), (13, 93, 13), (88, 144, 144)],
+    [(1, 100, 432), (10, 14, 12), (13, 93, 13), (88, 144, 144)],
 )
-def test_complex(n_boards, sizeX, sizeY):
-    bg = BoardsGenerator()
+def test_noise_generator(n_boards, sizeX, sizeY):
+    bg = BoardsGenerator("tests/testTree.root")
     bg.GenerateBoards(n_boards, n_rings=(1, 1), size=(sizeX, sizeY), noise_level=1e3)
     boards, sizes = bg.GetBoards()
-    print(boards)
     assert sizes[0] == sizeX
     assert sizes[1] == sizeY
-    if n_boards == 0:
-        assert boards.size == 0
-    else:
-        assert boards.shape[0] == 5
-        assert (boards[4, :]).max() == n_boards - 1
+    assert boards.shape[1] == 5
+    assert (boards[4, :]).max() == n_boards - 1
